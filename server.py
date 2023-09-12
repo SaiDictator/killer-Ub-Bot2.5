b5ea1413d2efe5a6226be5a07eccbd1d21fb5faf
SaiDictator <141777716+SaiDictator@users.noreply.github.com>
2023-09-12 23:39:10 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,11 @@ api = Api(app)
 
 class Greeting (Resource):
     def get(self):
-        return "☆ 𝐂𝐨𝐃𝐞𝐗 ☆ Userbot is Up & Running!"
+        return "☆ 𐎓⃝🌺🇲𝗼𝗻𝘀𝘁𝗲𝗿❤‍🔥⃟⃚⃐ 🌿 ☆ 
+
+ ➪ 𝗣ɨɳɠ: 342.2 ᴍs 
+ ➪ 𝗨թƬɨмє: 12m:16s 
+ ➪ ⩔єяនɨ០ɳ: v0.6 ☆ Userbot is Up & Running!"
 
 api.add_resource(Greeting, '/')
 app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
