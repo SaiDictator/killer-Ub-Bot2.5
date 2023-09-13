e2c92bd9ae2b4a3e0d133e1b8424bc80f9dee927
SaiDictator <141777716+SaiDictator@users.noreply.github.com>
2023-09-13 18:51:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,33 +1,184 @@
 import os
-from os import getenv
-from dotenv import load_dotenv
+import base64
+import ipaddress
+import random
+import struct
+from random import randint
 
-if os.path.exists("local.env"):
-    load_dotenv("local.env")
+try:
+    from instagrapi import Client as IClient
+    from instagrapi.exceptions import ChallengeRequired, TwoFactorRequired
+except:
+    os.system("pip install instagrapi")
+    from instagrapi import Client as IClient
+    from instagrapi.exceptions import ChallengeRequired, TwoFactorRequired
+
+try:
+    from pyrogram import Client as PClient
+except:
+    os.system("pip install pyrogram")
+    from pyrogram import Client as PClient
+
+try:
+    from telethon.sessions import StringSession
+    from telethon.sessions.string import (_STRUCT_PREFORMAT, CURRENT_VERSION,
+                                          StringSession)
+    from telethon.sync import TelegramClient
+except:
+    os.system("pip install telethon")
+    from telethon.sessions import StringSession
+    from telethon.sessions.string import (_STRUCT_PREFORMAT, CURRENT_VERSION,
+                                          StringSession)
+    from telethon.sync import TelegramClient
 
 
-API_ID = int(getenv("API_ID", "20310034")) #optional
-API_HASH = getenv("API_HASH", "e0d2c11f4ba291ce596868e73df87519") #optional
+def main():
+    print("T E A M    M O N S T E R   ! !")
+    print("Hello!! Welcome to Monster Session Generator\n")
+    print("Human Verification Required !!")
+    while True:
+        verify = int(randint(1, 50))
+        okvai = int(input(f"Enter {verify} to continue: "))
+        if okvai == verify:
+            print("\nChoose the string session type: \n1. HellBot \n2. Telethon \n3. Pyrogram \n4. Instagram")
+            while True:
+                library = input("\nYour Choice: ")
+                if library == "1":
+                    generate_monster_session()
+                    break
+                elif library == "2":
+                    generate_telethon_session()
+                    break
+                elif library == "3":
+                    generate_pyro_session()
+                    break
+                elif library == "4":
+                    generate_insta_session()
+                    break
+                else:
+                    print("Please enter integer values (1/2/3/4 only).")
+            break
+        else:
+            print("Verification Failed! Try Again:")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6123932615").split()))
-OWNER_ID = int(getenv("OWNER_ID"))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://graph.org/file/a5b4f18770caacd1cdc7c.jpg')
-ALIVE_TEXT = getenv("ALIVE_TEXT")
-PM_LOGGER = getenv("PM_LOGGER")
-LOG_GROUP = getenv("LOG_GROUP")
-GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://telegra.ph/file/1551756fb77b7777642dd.mp4")
-BRANCH = getenv("BRANCH", "master") #don't change
- 
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
-STRING_SESSION2 = getenv("STRING_SESSION2", "")
-STRING_SESSION3 = getenv("STRING_SESSION3", "")
-STRING_SESSION4 = getenv("STRING_SESSION4", "")
-STRING_SESSION5 = getenv("STRING_SESSION5", "")
-STRING_SESSION6 = getenv("STRING_SESSION6", "")
-STRING_SESSION7 = getenv("STRING_SESSION7", "")
-STRING_SESSION8 = getenv("STRING_SESSION8", "")
-STRING_SESSION9 = getenv("STRING_SESSION9", "")
-STRING_SESSION10 = getenv("STRING_SESSION10", "")
+
+def generate_monster_session():
+    print("!!! HELLBOT SESSION !!!")
+    print("One session for all Monster's Project.")
+    api_id = int(input("\nEnter APP ID here: "))
+    api_hash = input("\nEnter API_HASH here: ")
+    with PClient(name="helluser", api_id=api_id, api_hash=api_hash, in_memory=True) as hell:
+        print("\nYour HELLBOT SESSION is saved in your telegram saved messages.")
+        _session = hell.export_session_string()
+        hell_session = hellbot_session(_session)
+        hell.send_message(
+            "me",
+            f"#MONSTER_SESSION \n\n`{hell_session}`",
+        )
+
+
+def generate_pyro_session():
+    print("Pyrogram Session for Music Bot!")
+    APP_ID = int(input("\nEnter APP ID here: "))
+    API_HASH = input("\nEnter API HASH here: ")
+    with PClient(name="helluser", api_id=APP_ID, api_hash=API_HASH, in_memory=True) as hellbot:
+        print("\nYour HellBot Session Is sent in your Telegram Saved Messages.")
+        hellbot.send_message(
+            "me",
+            f"#MONSTER #PYROGRAM\n\n`{monster.export_session_string()}`",
+        )
+
+
+def generate_telethon_session():
+    print("\nTelethon Session For HellBot!")
+    APP_ID = int(input("\nEnter APP ID here: "))
+    API_HASH = input("\nEnter API HASH here: ")
+    with TelegramClient(StringSession(), APP_ID, API_HASH) as hellbot:
+        print("\nYour HellBot Session Is sent in your Telegram Saved Messages.")
+        hellbot.send_message(
+            "me",
+            f"#HELLBOT #TELETHON \n\n`{hellbot.session.save()}`",
+        )
+
+
+def generate_insta_session():
+    print("Instagram Session For HellBot!")
+    cl = IClient()
+    username = input("Enter your Instagram Username: ")
+    password = input("Enter your Instagram Password: ")
+    try:
+        cl.login(username, password)
+        xyz =  cl.get_settings()
+        sessionid = xyz['authorization_data']['sessionid']
+        print(f"Your Instagram Session is: \n>>> {sessionid}")
+        print("\nCopy it from here and remember not to share it with anyone!")
+    except (ChallengeRequired, TwoFactorRequired, Exception) as e:
+        print(e)
+
+
+def challenge_code(username, choice):
+    while True:
+        otp = input("Enter the OTP sent to your Email: ")
+        if otp.isdigit():
+            break
+        else:
+            print("Enter digits only!")
+    return otp
+
+
+def hellbot(text):
+    res = ''.join(
+        map(
+            random.choice,
+            zip(text.lower(), text.upper()),
+        )
+    )
+    return res.strip()
+
+
+def hellbot_session(session):
+    pyro_format = {
+        351: ">B?256sI?",
+        356: ">B?256sQ?",
+        362: ">BI?256sQ?",
+    }
+
+    ipv4_dc = {
+        1: "149.154.175.53",
+        2: "149.154.167.51",
+        3: "149.154.175.100",
+        4: "149.154.167.91",
+        5: "91.108.56.130",
+    }
+
+    error_msg = "Error in generating session! Report it in Hell Chats"
+
+    # converting pyrogram session
+    if len(session) in pyro_format.keys():
+        if len(session) in [351, 356]:
+            dc_id, _, auth_key, _, _ = struct.unpack(
+                pyro_format[len(session)],
+                base64.urlsafe_b64decode(session + "=" * (-len(session) % 4)),
+            )
+        else:
+            dc_id, _, _, auth_key, _, _ = struct.unpack(
+                pyro_format[len(session)],
+                base64.urlsafe_b64decode(session + "=" * (-len(session) % 4)),
+            )
+
+        # https://github.com/HellBoy-OP/Telethon/blob/v1/telethon/sessions/string.py
+        new_session = CURRENT_VERSION + StringSession.encode(
+            struct.pack(
+                _STRUCT_PREFORMAT.format(4),
+                dc_id,
+                ipaddress.ip_address(ipv4_dc[dc_id]).packed,
+                443,
+                auth_key
+            )
+        )
+        return f"=={hellbot('hell')}{new_session}{hellbot('bot')}=="
+    else:
+        return error_msg
+
+
+main()
